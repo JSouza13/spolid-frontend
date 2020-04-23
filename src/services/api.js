cae9ad960af248486fe68dc503406a48abcfea5d
João Vitor de Souza <joao_souza@univali.br>
2020-04-23 17:11:13 -0300

    variáveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
+import 'dotenv/config';
 import axios from 'axios';
 
+if(process.env.NODE_ENV === 'development') {
+  url = 'http://localhost:3333'
+}
+
+if(process.env.NODE_ENV === 'production') {
+  url = ''
+}
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: url,
 });
 
 export default api;
