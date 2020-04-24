d8a833ef1377d7e6959ca48d2de55c9ad2525460
Joao Vitor <jooao_souza@hotmail.com>
2020-04-24 07:09:59 -0300

    correçao endereço api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://spolid-back.herokuapp.com/',
+  baseURL: 'https://spolid-back.herokuapp.com',
 });
 
 export default api;
